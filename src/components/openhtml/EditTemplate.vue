0b177346d78c5a3b22c7c35043fd76a474af4970
JachinYin <1106025235@qq.com>
2019-03-20 23:39:50 +0800

    编辑模板

--- src/components/openhtml/EditTemplate.vue
+++ src/components/openhtml/EditTemplate.vue
@@ -10,11 +10,18 @@
           <img :src="tempData.imgUrl" alt="">
         </div>
         <div class="content">
-          <h3>{{tempData.title}}</h3>
-          <p>{{tempData.content}}</p>
-          <p>{{tempData.content}}</p>
-          <p>{{tempData.content}}</p>
-          <p>{{tempData.content}}</p>
+          <div class="item">
+            <span class="keyClass">标题</span>
+            <span class="valClass">{{tempData.title}}</span></div>
+          <div class="item">
+            <span class="keyClass">关键词</span>
+            <span class="valClass">{{tempData.keyWd}}</span></div>
+          <div class="item">
+            <span class="keyClass">行业信息</span>
+            <span class="valClass">{{tempData.info}}</span></div>
+          <div class="item">
+            <span class="keyClass">内容</span>
+            <span class="valClass">{{tempData.content}}</span></div>
         </div>
       </div>
       <div class="footer">
@@ -70,6 +77,28 @@
   .container img{
     width: 180px;
   }
+
+
+ .item{
+   margin: 10px 10px;
+   padding: 6px 0;
+   text-align: left;
+   border-bottom: 1px #f1f1f1 solid;
+ }
+ .item .keyClass{
+   /*font-size: 20px;*/
+   /*font-weight: bold;*/
+   display: inline-block;
+   padding-left: 12px;
+   width: 80px;
+   color: #909399;
+ }
+ .item .valClass{
+   display: inline-block;
+   width: 275px;
+ }
+
+
   .footer{
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
     height: 50px;
